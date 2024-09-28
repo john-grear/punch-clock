8ce99352afd38c936e6977ab6b42d75988524911
John <piquagrearj@gmail.com>
2024-09-28 09:56:24 -0400

    Removed temp page.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "054f76a0f941c734ad075b47fa5282bc",
+"flutter_bootstrap.js": "cb6e99e81f595376970a6a65cab92652",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "20b84937d5dad97c56b3ec02f290d6bf",
-"/": "20b84937d5dad97c56b3ec02f290d6bf",
-"main.dart.js": "49ef928224b2af7e7dbb8f4e9bbc6dd1",
+"index.html": "9934fb2091b3768fae1757445e452a29",
+"/": "9934fb2091b3768fae1757445e452a29",
+"main.dart.js": "2b9fa13a89e1f7e595d242cb20e1b02f",
 "manifest.json": "d3391756ca14db219e95c81ca9c4223d",
 "version.json": "07bef7b35ba1ff5f84b3c512763efdd9"};
 // The application shell files that are downloaded before a service worker can
