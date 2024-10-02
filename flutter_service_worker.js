3ca5daaefc6d31c5176ff11b5c1352d5b44b65bb
John <piquagrearj@gmail.com>
2024-10-02 07:08:20 -0400

    Commented everything in punch_clock.dart out except for title to see if this file is the issue entirely.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b677d28277ec1130b69c69d6b91a4382
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "180a528aaf63c7b673915ca392d6b11e",
+"flutter_bootstrap.js": "87ef3ae421ad15793a09032699e851b3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "96e1331622c8bc5b615518b744dc7bdc",
-"/": "96e1331622c8bc5b615518b744dc7bdc",
-"main.dart.js": "3fe24548141a5834387f11154fd7ea23",
+"index.html": "918b7390aed308e67a2682d88b4f2ac5",
+"/": "918b7390aed308e67a2682d88b4f2ac5",
+"main.dart.js": "f2a5725fda5a451f181927388c801294",
 "manifest.json": "d3391756ca14db219e95c81ca9c4223d",
 "version.json": "07bef7b35ba1ff5f84b3c512763efdd9"};
 // The application shell files that are downloaded before a service worker can
